17a600d26f987b2e4a626fac9bd51acf5f6e3c2f
sxg18 <leoparddesneiges1@protonmail.com>
2024-07-20 02:46:39 +0300

    qol things

--- StormDistanceCalculator.py
+++ StormDistanceCalculator.py
@@ -7,7 +7,7 @@ def calculate_distance(time_in_seconds):
     return distance_in_miles, distance_in_km
 
 
-def calculate_distance_in_gui():
+def calculate_distance_in_gui(event=None):
     time_elapsed = float(time_entry.get())
     distance_in_miles, distance_in_km = calculate_distance(time_elapsed)
     result_label.config(text=f"The distance of the lightning strike is {distance_in_miles} miles (or {distance_in_km} kilometers).")
@@ -20,6 +20,8 @@ time_label.pack()
 
 time_entry = tk.Entry(window, width=20)
 time_entry.pack()
+time_entry.focus_set()
+time_entry.bind("<Return>", calculate_distance_in_gui)
 
 calculate_button = tk.Button(window, text="Calculate", command=calculate_distance_in_gui)
 calculate_button.pack()
